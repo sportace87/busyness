806cb769e64281a20d98c107defa911ec0430248
Tolev <atolev@147dda318b97.ant.amazon.com>
2021-02-01 17:55:06 +0100

    Added elgato key lights

--- scripts/checkIsOnCall.sh
+++ scripts/checkIsOnCall.sh
@@ -4,10 +4,14 @@ isTeamsCall=$(lsof | grep "/Applications/Microsoft Teams.app/Contents/MacOS/Team
 if [ -n "$isChimeCall" ] || [ -n "$isTeamsCall" ]; then
     isCameraOn=$(node ~/Workspace/busyness/main.js)
     if $isCameraOn; then
+        curl -X PUT -H "Content-Type: application/json" -d '{"numberOfLights": 1,"lights": [{"on": 1,"brightness": 40,"temperature": 190}]}' http://192.168.178.45:9123/elgato/lights
+        curl -X PUT -H "Content-Type: application/json" -d '{"numberOfLights": 1,"lights": [{"on": 1,"brightness": 40,"temperature": 190}]}' http://192.168.178.44:9123/elgato/lights
         busylight --all on red
     else
-        busylight --all on orange
+        busylight --all on blue
     fi
 else
+    curl -X PUT -H "Content-Type: application/json" -d '{"numberOfLights": 1,"lights": [{"on": 0,"brightness": 40,"temperature": 190}]}' http://192.168.178.45:9123/elgato/lights
+    curl -X PUT -H "Content-Type: application/json" -d '{"numberOfLights": 1,"lights": [{"on": 0,"brightness": 40,"temperature": 190}]}' http://192.168.178.44:9123/elgato/lights
     busylight --all on green
 fi
